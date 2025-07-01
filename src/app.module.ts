fbf3be519436b470c88c3c78da81282188be8224
Ulugbek Tojikulov <cookie.pechehbkov@mail.ru>
2025-07-01 23:43:30 +0500

    holy fcking messed up/ dont jump between different projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [AuthModule, UsersModule]
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthDto } from './dto/auth.dto'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	@Post('login')
+	async login(@Body() dto: AuthDto, @Response({ passthrough: true }) res) {
+		return await this.authService.login(dto, res)
+	}
+}
